f50e729752f29b9b474bddb9995d02b4810f78b3
karteek <152055912+karteeksai1@users.noreply.github.com>
2026-06-27 13:31:31 +0530

    Add configuration file for app settings

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+  appName: "HireGraph",
+  port: 8000,
+  database: {
+    host: "production-db.company.com",
+    user: "root",
+    password: "secure_password",
+  },
+};
+
+module.exports = config;
